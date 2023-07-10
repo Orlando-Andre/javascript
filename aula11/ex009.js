e3947dd08d2d699255c02517d4df710901a9416b
Orlando André Souza Neto <orlandoandre019@gmail.com>
2023-07-10 17:57:33 -0300

    exercício 11 easy

---
+++ aula11/ex009.js
@@ -0,0 +1,11 @@
+var pais = 'França'
+
+console.log(`Vivendo em ${pais}`)
+
+if(pais == 'Brasil') { //Condição composta
+
+    console.log('Você é brasileiro(a)!')
+} else {
+
+    console.log('Você é estrangeiro(a)!')
+}
